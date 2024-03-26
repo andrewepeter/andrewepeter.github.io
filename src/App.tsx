f46b44a8bb313a61cb2c8019d08e6d3f4ccbc25a
andrewepeter <64112518+andrewepeter@users.noreply.github.com>
2024-03-26 15:11:52 -0400

    updating site for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,31 @@ import Button from "./components/Button";
 import "./App.css"
 //import { Link } from 'react-router-dom';
 
+/*
+Ideas:
+
+-Have a biosection from LinkedIn
+-Projects
+-Contact
+-Resume
+-Cerficiations
+-gmail
+-github
+
+*/
+
+
 function App() {
   const [alertVisible, setAlertvisibility] = useState(false)
   return (
-    <div className="background-lightgray">
+    <div >
        {alertVisible && <Alert onClose={()=> setAlertvisibility(false)}>My Alert</Alert>}
       <Button onClick={() => setAlertvisibility(true)}>Primary</Button>
       <Link linkString={"https://www.linkedin.com/posts/andrewepeter_coding-unity3d-gamedev-activity-7059231125474332673-t8Sc?utm_source=share&utm_medium=member_desktop"}>LinkedIn</Link>
       <Link linkString={"https://github.com/andrewepeter"}>Github</Link>
-      <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7059230994167443456?compact=1" height="399" width="710" frameBorder="0" allowfullscreen="yes" title="Embedded post"></iframe>
+      <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7059230994167443456?compact=1" height="1000" width="900" frameBorder="0" allowfullscreen="yes" title="Embedded post"></iframe>
       <Link linkString={"https://play.unity.com/mg/other/webgl-builds-330917"}>Click here to play for free!</Link>
-      <p className="text-blue">hey</p>
+      <p className="center">hey</p>
     </div>
   );
 }
